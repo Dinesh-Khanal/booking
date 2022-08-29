54d6380b24e58f4f486e0c25f393885e163103fc
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2022-08-29 22:30:13 +0545

    preliminary designing finished

---
+++ src/images.ts
@@ -0,0 +1,11 @@
+const images = [
+  "https://cf.bstatic.com/xdata/images/hotel/max1280x900/377029908.jpg?k=2473dfbcce053be4d780b4a69586747c9155696280c35ad5b5a8a3440018f01d&o=&hp=1",
+  "https://cf.bstatic.com/xdata/images/hotel/max1024x768/377029232.jpg?k=38d732b481be349f0a66f2915424ff5b695374ac32ece17aefac7ad86bd94715&o=&hp=1",
+  "https://cf.bstatic.com/xdata/images/hotel/max1280x900/377030058.jpg?k=a6645506aa2b9ad6626d4ac30d6a9b9c8b35b8d9c8dfd74ab4292f86dabf6787&o=&hp=1",
+  "https://cf.bstatic.com/xdata/images/hotel/max1280x900/377029861.jpg?k=3d1bd70703137e10eb50e21308a11c0c5d862702c7eba235e67a8dc95622f3ac&o=&hp=1",
+  "https://cf.bstatic.com/xdata/images/hotel/max1280x900/377030093.jpg?k=41ded9cae63ad6ee3ef4c3e3c368c72fe81b82cfe65452632cf0f02803606be6&o=&hp=1",
+  "https://cf.bstatic.com/xdata/images/hotel/max1280x900/377040014.jpg?k=1e2654085063b4730dc3bb25a14c883f09df5496766e517ed5259b62d5d115a9&o=&hp=1",
+  "https://cf.bstatic.com/xdata/images/hotel/max1280x900/378795859.jpg?k=437431e06a097f7a7da53ca25d2bb6fbbcdcb88ef7aadfa9e644630ad6f655c4&o=&hp=1",
+  "https://cf.bstatic.com/xdata/images/hotel/max1280x900/378796169.jpg?k=82e7b67db6f0f5cf5b740614a32a7fc99702a57916d7110752869039c0ba269e&o=&hp=1",
+];
+export default images;
--- src/pages/hotel/Hotel.tsx
+++ src/pages/hotel/Hotel.tsx
@@ -1,10 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../../components/header/Header";
 import "./hotel.css";
-
+import { MdLocationPin } from "react-icons/md";
+import {
+  FaArrowCircleLeft,
+  FaArrowCircleRight,
+  FaTimesCircle,
+} from "react-icons/fa";
+import FooterEmail from "../../components/FooterEmail";
+import BottomFooter from "../../components/BottomFooter";
+import images from "../../images";
 const Hotel = () => {
+  const [open, setOpen] = useState(false);
+  const [openImageId, setOpenImageId] = useState(0);
+
+  const handleClick = (i: number) => {
+    setOpenImageId(i);
+    setOpen(true);
+  };
+
+  const handleArrow = (arrow: string) => {
+    if (arrow === "l") {
+      openImageId > 0
+        ? setOpenImageId(openImageId - 1)
+        : setOpenImageId(images.length - 4);
+    } else if (arrow === "r") {
+      openImageId < images.length - 4
+        ? setOpenImageId(openImageId + 1)
+        : setOpenImageId(0);
+    }
+  };
+
   return (
-    <div>
-      <h2>Hotel Page</h2>
+    <div className="Hotel">
+      <Header type="list" />
+      <div className="HotelContainer">
+        <div className="HotelWraper">
+          {open && (
+            <div className="hotelSlider">
+              <FaArrowCircleLeft onClick={() => handleArrow("l")} />
+              <span className="hotelCloseBtn" onClick={() => setOpen(false)}>
+                <FaTimesCircle onClick={() => handleArrow("r")} />
+              </span>
+              <img src={images[openImageId]} alt="" />
+              <FaArrowCircleRight onClick={() => handleArrow("r")} />
+            </div>
+          )}
+          <div className="hotelHeader">
+            <a href="/#">Info and prices</a>
+            <a href="/#facilities">Facilities</a>
+            <a href="/#">House rules</a>
+            <a href="/#">Guest reviews</a>
+          </div>
+          <h1 className="hotelTitle">Kathmandu Aagantuk Hotel</h1>
+          <div className="hotelAddress">
+            <MdLocationPin />
+            <span>
+              Lekhnath Marg, Thamel, Thamel, 44600 Kathmandu , best location{" "}
+              <a href="https://goo.gl/maps/4QxTT61ACCnRq72h6">View in map</a>
+            </span>
+          </div>
+          <div className="hotelImages">
+            <div className="hotelImagesTop">
+              <div className="hotelSideImage">
+                <img src={images[1]} alt="" />
+                <img src={images[2]} alt="" />
+              </div>
+              <div className="hotelMainImage">
+                <img src={images[0]} alt="" />
+              </div>
+            </div>
+            <div className="hotelImagesBottom">
+              {images.slice(3, 8).map((image, i) => (
+                <img
+                  key={i}
+                  src={image}
+                  alt=""
+                  onClick={() => handleClick(i)}
+                />
+              ))}
+            </div>
+          </div>
+          <FooterEmail />
+          <BottomFooter />
+        </div>
+      </div>
     </div>
   );
 };
